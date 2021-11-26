9efb24624db59201c21ec53e7f361881cfebf638
sulei <779647751@qq.com>
2021-11-26 21:08:45 +0800

    feat: add tableList components 🍉

--- src/client/components/Layouts/Index.tsx
+++ src/client/components/Layouts/Index.tsx
@@ -1,13 +1,16 @@
 import React, { useContext } from 'react'
 import { useLocation, useHistory, Link } from 'react-router-dom'
 import { Icon as LegacyIcon } from '@ant-design/compatible'
-import { CopyrightOutlined, GithubOutlined, ExportOutlined, FacebookOutlined } from '@ant-design/icons'
-import { Layout, Breadcrumb, Row, Col, Menu, Popconfirm, Tag } from 'antd'
+import { CopyrightOutlined, GithubOutlined, ExportOutlined, FacebookOutlined, DownOutlined, GlobalOutlined } from '@ant-design/icons'
+import { Layout, Breadcrumb, Row, Col, Menu, Popconfirm, Tag, Dropdown } from 'antd'
 import { FormattedMessage } from 'react-intl' /* reßact-intl imports */
 import { useRecoilState } from 'recoil'
 import { ISettingProps } from '@models/settingModels'
 import { settingStateSelector } from '@recoil/selector/settingSelector'
 import { menus, findActiveMenu } from '@router/index'
+
+// Components
+import HeaderDropdown from '@components/HeaderDown'
 
 import zhCN from '@language/zh'
 import enUS from '@language/en'
@@ -18,6 +21,7 @@ const message = {
 const { Sider, Content } = Layout
 const { SubMenu } = Menu
 import './Index.css'
+import { Components } from 'antd/lib/date-picker/generatePicker'
 
 export const Layouts: React.FC<{ headerAppender?: React.ReactNode }> = props => {
   const { headerAppender, children } = props
@@ -34,8 +38,8 @@ export const Layouts: React.FC<{ headerAppender?: React.ReactNode }> = props => 
     })
   }
 
-  const toggleLanguage = () => {
-    const locale = setting.locale === 'zh' ? 'en' : 'zh'
+  const toggleLanguage = (key: string) => {
+    const locale = key === 'zh' ? 'zh' : 'en'
     setSettingAction({
       ...setting,
       locale,
@@ -81,6 +85,26 @@ export const Layouts: React.FC<{ headerAppender?: React.ReactNode }> = props => 
         })}
     </Menu>
   )
+
+  const onClick = ({ key }: any) => {
+    toggleLanguage(key)
+  }
+
+  const languageMenu = (
+    <Menu onClick={onClick}>
+      {[
+        { label: '中文', value: 'zh' },
+        { label: '英文', value: 'en' }
+      ].map(item => {
+        return (
+          <Menu.Item key={item.value} className={setting && setting.locale === item.value && 'ant-dropdown-menu-item-selected'}>
+            <FormattedMessage id={item.value} defaultMessage={item.value}></FormattedMessage>
+          </Menu.Item>
+        )
+      })}
+    </Menu>
+  )
+
   return (
     <Layout className="container">
       <Sider className="asider" trigger={null} collapsible={true} collapsed={setting.collapsed}>
@@ -107,22 +131,27 @@ export const Layouts: React.FC<{ headerAppender?: React.ReactNode }> = props => 
       </Sider>
       <Layout className="main">
         <header>
-          <Row gutter={24}>
-            <Col span={8}>
+          <Row gutter={24} style={{ width: '100%' }}>
+            <Col span={2}>
               <LegacyIcon className="trigger" type={setting.collapsed ? 'menu-unfold' : 'menu-fold'} onClick={toggleCollapse} />
             </Col>
-            <Col span={8}>
-              <Tag icon={<FacebookOutlined />} color="#3b5999" onClick={() => toggleLanguage()}>
-                <FormattedMessage id={setting.locale} defaultMessage={setting.locale}></FormattedMessage>
-              </Tag>
-            </Col>
-            <Col span={8} style={{ textAlign: 'right' }}>
+            <Col span={21}></Col>
+            <Col span={1} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
               <div className="info">
                 <a className="github" href="https://github.com/fantasticit/wipi" target="_blank" rel="noreferrer">
                   <GithubOutlined />
                 </a>
                 {/* <UserInfo /> */}
               </div>
+              <Dropdown overlay={languageMenu} placement={'bottomRight'} arrow>
+                <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                  <svg viewBox="0 0 24 24" focusable="false" width="20px" height="20px" fill="currentColor" aria-hidden="true">
+                    <path d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M12.87 15.07l-2.54-2.51.03-.03c1.74-1.94 2.98-4.17 3.71-6.53H17V4h-7V2H8v2H1v1.99h11.17C11.5 7.92 10.44 9.75 9 11.35 8.07 10.32 7.3 9.19 6.69 8h-2c.73 1.63 1.73 3.17 2.98 4.56l-5.09 5.02L4 19l5-5 3.11 3.11.76-2.04zM18.5 10h-2L12 22h2l1.12-3h4.75L21 22h2l-4.5-12zm-2.62 7l1.62-4.33L19.12 17h-3.24z "></path>
+                  </svg>
+                </a>
+              </Dropdown>
+              {/* <HeaderDropdown overlay={languageMenu} trigger={['click']} /> */}
             </Col>
           </Row>
         </header>
@@ -148,7 +177,7 @@ export const Layouts: React.FC<{ headerAppender?: React.ReactNode }> = props => 
           <footer>
             <div className="">
               <p>
-                Copyright <CopyrightOutlined /> {new Date().getFullYear()} Designed by  
+                Copyright <CopyrightOutlined /> {new Date().getFullYear()} Designed by
                 {/* <a href="https://github.com/fantasticit/wipi" target="_blank" rel="noreferrer">
                     Fantasticit.
                   </a> */}
--- src/client/pages/Visit/Visit.tsx
+++ src/client/pages/Visit/Visit.tsx
@@ -1,6 +1,309 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FormattedMessage } from 'react-intl'
+import { Space, Tooltip, Table } from 'antd'
+import { TableList } from '@components/TableList/TableList'
+import { InfoCircleOutlined } from '@ant-design/icons'
 
 const VisitPage: React.FC = () => {
-  return <div>visit</div>
+  const [loading, setLoading] = useState(false)
+  const [selectedRowKeys, setSelectedRowKeys] = useState<number[]>([])
+  const [sortedInfo, setSortedInfo] = useState({
+    columnKey: 'age',
+    order: 'descend'
+  })
+
+  const handlePaginationCallback = (page: number, pageSize?: number | undefined) => {
+    console.log(page, pageSize)
+    setLoading(false)
+  }
+
+  const handleReloadCallback = () => {
+    console.log('刷新了')
+  }
+
+  const handleTableChangeCallback = (pagination, filters, sorter, extra) => {
+    setSortedInfo({
+      columnKey: 'age',
+      order: sorter.order
+    })
+  }
+
+  const onSelectChange = (selectedRowKeys, selectedRows) => {
+    setSelectedRowKeys(selectedRowKeys)
+  }
+
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+    selections: [
+      Table.SELECTION_ALL,
+      Table.SELECTION_INVERT,
+      Table.SELECTION_NONE,
+      {
+        key: 'odd',
+        text: 'Select Odd Row',
+        onSelect: (changableRowKeys: any) => {
+          let newSelectedRowKeys = []
+          newSelectedRowKeys = changableRowKeys.filter((key: any, index: number) => {
+            if (index % 2 !== 0) {
+              return false
+            }
+            return true
+          })
+          setSelectedRowKeys(newSelectedRowKeys)
+        }
+      },
+      {
+        key: 'even',
+        text: 'Select Even Row',
+        onSelect: (changableRowKeys: any[]) => {
+          let newSelectedRowKeys = []
+          newSelectedRowKeys = changableRowKeys.filter((key: any, index: number) => {
+            if (index % 2 !== 0) {
+              return true
+            }
+            return false
+          })
+          setSelectedRowKeys(newSelectedRowKeys)
+        }
+      }
+    ]
+  }
+
+  return (
+    <div>
+      visit
+      <TableList
+        paginationCallback={handlePaginationCallback}
+        tableChangeCallback={handleTableChangeCallback}
+        reloadCallback={handleReloadCallback}
+        loading={loading}
+        total={100}
+        page={1}
+        pageSize={10}
+        rowSelection={rowSelection}
+        scroll={{ x: 1300 }}
+        columns={[
+          {
+            title: (
+              <div>
+                标题&nbsp;
+                <Tooltip placement="top" title={'我该显示点啥'}>
+                  <InfoCircleOutlined />
+                </Tooltip>
+              </div>
+            ),
+            fixed: 'left',
+            dataIndex: 'url'
+          },
+          {
+            title: 'Ip',
+            dataIndex: 'ip'
+          },
+          {
+            title: '浏览器',
+            dataIndex: 'browser'
+          },
+          {
+            title: '内核',
+            dataIndex: 'engine'
+          },
+          {
+            title: '操作系统',
+            dataIndex: 'os'
+          },
+          {
+            title: '设备',
+            dataIndex: 'device'
+          },
+          {
+            title: '地址',
+            dataIndex: 'address'
+          },
+          {
+            title: '访问量',
+            dataIndex: 'count'
+          },
+          {
+            title: '访问时间',
+            dataIndex: 'updateAt',
+            sortDirections: ['descend', 'ascend'],
+            sorter: () => {
+              return true
+            },
+            sortOrder: sortedInfo.order
+          },
+          {
+            title: '操作',
+            dataIndex: 'action',
+            width: 150,
+            align: 'center',
+            fixed: 'right',
+            render: () => {
+              return (
+                <Space>
+                  <a>删除</a>
+                </Space>
+              )
+            }
+          }
+        ]}
+        data={[
+          {
+            id: '0e51b119-3dfa-4803-8401-ff2a4319ac48',
+            ip: '66.23.193.243',
+            userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36',
+            url: 'http://www.humanweaknesses.com:5003/view',
+            count: 1,
+            address: '美国 加利福尼亚',
+            browser: 'Chrome 96.0.4664.55',
+            engine: 'Blink 96.0.4664.55',
+            os: 'Mac OS 10.15.7',
+            device: '',
+            createAt: '2021-11-26T03:58:53.053Z',
+            updateAt: '2021-11-26T03:58:53.053Z'
+          },
+          {
+            id: '7ea5fe30-6c80-4df5-ad27-a5fdc2d1ecac',
+            ip: '66.23.193.242',
+            userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36',
+            url: 'http://www.humanweaknesses.com:5003/view',
+            count: 1,
+            address: '美国 加利福尼亚',
+            browser: 'Chrome 96.0.4664.55',
+            engine: 'Blink 96.0.4664.55',
+            os: 'Mac OS 10.15.7',
+            device: '',
+            createAt: '2021-11-26T03:57:45.443Z',
+            updateAt: '2021-11-26T03:57:45.443Z'
+          },
+          {
+            id: 'a5d98740-e744-4b50-939d-ad329336c5db',
+            ip: '66.23.193.244',
+            userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36',
+            url: 'http://www.humanweaknesses.com:5003/login',
+            count: 1,
+            address: '美国 加利福尼亚',
+            browser: 'Chrome 96.0.4664.55',
+            engine: 'Blink 96.0.4664.55',
+            os: 'Mac OS 10.15.7',
+            device: '',
+            createAt: '2021-11-26T03:57:01.769Z',
+            updateAt: '2021-11-26T03:57:01.769Z'
+          },
+          {
+            id: '6a575719-3941-4857-8df2-f3f06dc8e8a3',
+            ip: '66.23.193.243',
+            userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36',
+            url: 'http://www.humanweaknesses.com:5003/',
+            count: 1,
+            address: '美国 加利福尼亚',
+            browser: 'Chrome 96.0.4664.55',
+            engine: 'Blink 96.0.4664.55',
+            os: 'Mac OS 10.15.7',
+            device: '',
+            createAt: '2021-11-26T03:56:56.041Z',
+            updateAt: '2021-11-26T03:56:56.041Z'
+          },
+          {
+            id: '838ba2e7-60cd-4497-b381-8fdaa8461248',
+            ip: '114.242.250.252',
+            userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1',
+            url: 'http://81.70.87.115:5003/file',
+            count: 2,
+            address: '中国 北京 北京市 联通',
+            browser: 'Mobile Safari 15.0',
+            engine: 'WebKit 605.1.15',
+            os: 'iOS 15.0.1',
+            device: 'Apple iPhone mobile',
+            createAt: '2021-11-25T11:09:34.707Z',
+            updateAt: '2021-11-25T11:09:37.000Z'
+          },
+          {
+            id: 'bbd20ff6-5112-4a39-9ac8-8ded53aa6a35',
+            ip: '114.242.250.252',
+            userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1',
+            url: 'http://81.70.87.115:5003/serviceManage/smsManage',
+            count: 4,
+            address: '中国 北京 北京市 联通',
+            browser: 'Mobile Safari 15.0',
+            engine: 'WebKit 605.1.15',
+            os: 'iOS 15.0.1',
+            device: 'Apple iPhone mobile',
+            createAt: '2021-11-25T11:09:25.254Z',
+            updateAt: '2021-11-25T11:09:42.000Z'
+          },
+          {
+            id: '28a670b9-573c-48e0-8b8a-ef0cf4b89433',
+            ip: '211.95.50.8',
+            userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+            url: 'http://humanweaknesses.com:5003/',
+            count: 1,
+            address: '中国 上海 上海市 联通',
+            browser: 'Chrome 87.0.4280.88',
+            engine: 'Blink 87.0.4280.88',
+            os: 'Mac OS 11.0.0',
+            device: '',
+            createAt: '2021-11-24T07:54:56.328Z',
+            updateAt: '2021-11-24T07:54:56.328Z'
+          },
+          {
+            id: 'c7bbaa23-6ddf-45c2-b0ee-43ee21735f8e',
+            ip: '43.224.45.108',
+            userAgent:
+              'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x63040026)',
+            url: 'http://www.humanweaknesses.com:5003/articles/0495b4e8-6a32-418c-9a79-c2eb72bac85a',
+            count: 1,
+            address: '中国 北京 北京市 皓宽网络',
+            browser: 'WeChat 7.0.20.1781',
+            engine: 'Blink 81.0.4044.138',
+            os: 'Windows 7',
+            device: '',
+            createAt: '2021-11-24T06:55:12.558Z',
+            updateAt: '2021-11-24T06:55:12.558Z'
+          },
+          {
+            id: '4537ed66-ffce-4b39-9b59-1a2089f28e90',
+            ip: '43.224.45.108',
+            userAgent:
+              'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x63040026)',
+            url: 'http://www.humanweaknesses.com:5003/',
+            count: 1,
+            address: '中国 北京 北京市 皓宽网络',
+            browser: 'WeChat 7.0.20.1781',
+            engine: 'Blink 81.0.4044.138',
+            os: 'Windows 7',
+            device: '',
+            createAt: '2021-11-24T06:54:51.705Z',
+            updateAt: '2021-11-24T06:54:51.705Z'
+          },
+          {
+            id: '2c0a4bdf-d1fd-4a63-abf7-43fe8183965c',
+            ip: '27.115.124.38',
+            userAgent:
+              'Mozilla/5.0 (Linux; U; Android 8.1.0; zh-CN; EML-AL00 Build/HUAWEIEML-AL00) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.108 baidu.sogo.uc.UCBrowser/11.9.4.974 UWS/2.13.1.48 Mobile Safari/537.36 AliApp(DingTalk/4.5.11) com.alibaba.android.rimet/10487439 Channel/227200 language/zh-CN',
+            url: 'http://www.humanweaknesses.com:5003/',
+            count: 1,
+            address: '中国 上海 上海市 联通',
+            browser: 'Android Browser 4.0',
+            engine: 'Blink 57.0.2987.108',
+            os: 'Android 8.1.0',
+            device: 'Huawei EML-AL00 mobile',
+            createAt: '2021-11-24T02:49:14.523Z',
+            updateAt: '2021-11-24T02:49:14.523Z'
+          }
+        ]}
+        buttonColumns={[
+          {
+            label: '新建',
+            value: 'create',
+            click: e => {
+              console.log(e, 'e....')
+            }
+          }
+        ]}
+      />
+    </div>
+  )
 }
 export default VisitPage
